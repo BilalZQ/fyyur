43cbf2d7a9527bb895af04d66d752bde9215478b
BilalZQ <bilq095@gmail.com>
2021-07-04 17:06:51 +0500

    refactor: added migration with updated artist & venue models

--- models.py
+++ models.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 #----------------------------------------------------------------------------#
 # App Config.
@@ -12,38 +13,62 @@ app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
-
-# TODO: connect to a local postgresql database
+migrate = Migrate(app, db)
 
 #----------------------------------------------------------------------------#
 # Models.
 #----------------------------------------------------------------------------#
 
 class Venue(db.Model):
-    __tablename__ = 'Venue'
+    """Venue Model."""
+    __tablename__ = 'venue'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    address = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    genres = db.Column('genres', db.ARRAY(db.String(120)), nullable=True)
+    website_link = db.Column(db.String(120), nullable=False)
+    seeking_talent = db.Column(db.Boolean, default=True)
+    seeking_description = db.Column(db.String(120))
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
+    def __repr__(self):
+        """
+        Return string representation for Venue model.
+
+        :return:
+        """
+        return f'Venue: {self.name}[{self.id}]'
+
 class Artist(db.Model):
-    __tablename__ = 'Artist'
+    """Artist model."""
+    __tablename__ = 'artist'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
+    genres = db.Column('genres', db.ARRAY(db.String(120)), nullable=False)
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    website_link = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean, default=False)
+    seeking_description = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        """
+        Return string representation for Artist model.
+
+        :return:
+        """
+        return f'Artist Id: {self.name}[{self.id}]'
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
